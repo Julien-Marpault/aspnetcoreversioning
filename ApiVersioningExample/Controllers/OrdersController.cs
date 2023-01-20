ecffe679d3874d9c5643dc3f7f42c2fbb3f4bac8
Julien Marpault <jmarpault@bimandco.com>
2023-01-20 15:50:06 +0100

    Partie 1

---
+++ ApiVersioningExample/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using ApiVersioningExample.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace ApiVersioningExample.Controllers;
+
+[ApiController]
+[Produces(MediaTypeNames.Application.Json)]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet("~/api/v1/[controller]")]
+    public IActionResult GetV1()
+    {
+        List<OrderDtoV1> orders = new List<OrderDtoV1> {
+            new() { Amount = 10M, ClientFirstname = "Bertrand", ClientLastname = "Dupuis", Number = 100000 },
+            new() { Amount = 100M, ClientFirstname = "Justine", ClientLastname = "Renard", Number = 100001 },
+            new() { Amount = 50M, ClientFirstname = "Alice", ClientLastname = "Wonderland", Number = 100002 }
+        };
+        return Ok(orders);
+    }
+
+    [HttpGet("~/api/v2/[controller]")]
+    public IActionResult GetV2()
+    {
+        List<OrderDtoV2> orders = new List<OrderDtoV2> {
+            new() { Amount = 10M, ClientName = "Bertrand Dupuis", Number = 100000 },
+            new() { Amount = 100M, ClientName = "Justine Renard", Number = 100001 },
+            new() { Amount = 50M, ClientName = "Alice Wonderland", Number = 100002 }
+        };
+        return Ok(orders);
+    }
+}
---
+++ ApiVersioningExample/Dto/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiVersioningExample.Dto;
+
+public abstract class OrderDtoBase
+{
+    public int Number { get; set; }
+    public decimal Amount { get; set; }
+}
+
+public class OrderDtoV1 : OrderDtoBase
+{
+    public string ClientLastname { get; set; }
+    public string ClientFirstname { get; set; }
+}
+
+public class OrderDtoV2 : OrderDtoBase
+{
+    public string ClientName { get; set; }
+}
